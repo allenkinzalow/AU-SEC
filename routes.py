53e395c0ac4b26789805505613e1295327268364
Jeramy Lochner <jerloch@gmail.com>
2018-04-08 16:49:17 -0500

    Added api command to get policies based on auth/policy/data ids, filterable by columns

--- routes.py
+++ routes.py
@@ -67,16 +67,18 @@ def create_policy():
     if not 'expiration' in request.json:
         request.json['expiration'] = 'NULL'
     if 'column_name' in request.json:
-        policy = Policy(request.json['data_id'], request.json['authorizers'], request.json['column_name'], request.json['table_name'], request.json['expiration'], request.json['policy_bitwise'])
+        group_id = create_auth_group(request.json['authorizers'])
+        policy = Policy(request.json['data_id'], request.json['authorizers'], group_id, request.json['column_name'], request.json['table_name'], request.json['expiration'], request.json['policy_bitwise'])
         db_session.add(policy)
         policy_ids.append(policy.policy_id)
-        group_ids.append(create_auth_group(request.json['authorizers']))
+        group_ids.append(group_id)
     else:
         for col in Patient.__table__.columns.keys():
-            policy = Policy(request.json['data_id'], request.json['authorizers'], col, request.json['table_name'], request.json['expiration'], request.json['policy_bitwise'])
+            group_id = create_auth_group(request.json['authorizers'])
+            policy = Policy(request.json['data_id'], request.json['authorizers'], group_id, col, request.json['table_name'], request.json['expiration'], request.json['policy_bitwise'])
             db_session.add(policy)
             policy_ids.append(policy.policy_id)
-            group_ids.append(create_auth_group(request.json['authorizers']))
+            group_ids.append(group_id)
 
     db_session.commit()
     return make_response(jsonify({'policy_ids': policy_ids, 'group_ids': group_ids, 'status': 'success'}), 200)
@@ -102,6 +104,26 @@ def edit_policy():
             Group.query.filter_by(group_id == old_group_id).delete()
     db_session.commit()
     return make_response(jsonify({'policy_ids': request.json['policy_ids'], 'status': 'success'}), 200)
+
+# {policy_ids: [id1, id2, ...], auth_ids: [id1, id2, ...], data_ids: [id1, id2, ...], column_names: [column1, column2, ...]}
+@routes.route('/api/policies/get_policies', methods=['GET'])
+def get_policies():
+    if 'policy_ids' in request.json:
+        policies = Policy.query.filter(Policy.policy_id.in_(request.json['policy_ids'])).all()
+    elif 'auth_ids' in request.json:
+        groups = Group.query.filter(Group.auth_id.in_(request.json['auth_ids'])).all()
+        group_ids = [group.group_id for group in groups]
+        policies = Policy.query.filter(Policy.group_id.in_(group_ids)).all()
+    elif 'data_ids' in request.json:
+        policies = Policy.query.filter(Policy.data_id.in_(request.json['data_ids'])).all()
+    else:
+        abort(400, {'message': 'One of (policy_ids, auth_ids, data_ids) needed'})
+
+    if 'column_names' in request.json:
+        policies = [policy.get_object() for policy in policies if policy.column_name in request.json['column_names']]
+    else:
+        policies = [policy.get_object() for policy in policies]
+    return make_response(jsonify({'policies': policies}))
 
 #{data: {column_names: column_data}, table_name: table, authorized_user: auth_id, row_id: id}
 @routes.route('/api/data/update', methods=['PUT'])
