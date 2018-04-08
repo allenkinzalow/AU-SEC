388e60e534f11664fb2b5ff1cb978336bf200b51
kinztechcom <allen@kinztech.com>
2018-04-08 15:06:13 -0500

    templating & basic schema design

--- static/js/aumed.js
+++ static/js/aumed.js
@@ -1,4 +1,34 @@
-$(document).ready(() => {
-  M.AutoInit();
 
+AUMed = {
+    load: function() {
+        
+        AUMed.UI.patients.populate();
+    },
+    UI: {
+        patients: {
+            _patients: [],
+            populate: function() {
+                // todo: use API instead of hardcoding.
+                this._patients.push(new AUMed.Schema.Patient({data_id: "12345", name: "Allen Kinzalow"}));
+                this._patients.push(new AUMed.Schema.Patient({data_id: "12346", name: "Haven Barnes"}));
+                $('#patients_table').html(
+                    this._patients.reduce(function(str, patient) {
+                        return str +  AUMed.Util.template($('#patient_entry_template').html(), {
+                            id: patient.data_id,
+                            name: patient.name
+                        });
+                    }, "")
+                );
+                M.AutoInit();
+            },
+        },
+        authorizations: {},
+        timeline: {},
+
+    },
+};
+
+$(document).ready(() => {
+    M.AutoInit();
+    AUMed.load();
 });
---
+++ static/js/util.js
@@ -0,0 +1,27 @@
+/**
+ * Utility functions.
+ */
+AUMed.Util = {
+    template: function(template, data) {
+        template = $.trim(template);
+        return template.replace(/%(\w*)%/g, function (m, key) {
+            return data.hasOwnProperty(key) ? data[key] : "";
+        });
+    },
+    api: function(params) {
+        $.ajax({
+            url: window.location.origin + "/api/" + params.url,
+            type: params.type ? params.type : "GET",
+            data: params.data ? JSON.stringify(params.data) : "",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: params.callback,
+            error: function(jqXHR, text, error) {
+                params.callback({
+                    error: text + " " + error,
+                });
+                console.log(text + " " + error);
+            },
+        });
+    },
+};
